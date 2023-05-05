ef929ae832161d3eeda054aa1a57fa480d0e038c
Arjunakurniawan <arjunakrn876@gmail.com>
2023-05-05 09:58:45 +0700

    first commit

---
+++ components/header.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=\, initial-scale=1.0">
+    <link rel="stylesheet" href="../assets/css/style.css">
+    <link rel="stylesheet" href="../assets/vendors/bootstrap/css/bootstrap.min.css">
+    <title>blog</title>
+</head>
+
+<body>
+
+    <nav class="navbar navbar-expand-lg bg-white py-3">
+        <div class="container">
+            <a href="" class="navbar-brand text-uppercase">Blogger</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#NavMenu">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navMenu">
+                <ul class="navbar-nav gap-3 mx-auto">
+                    <li class="nav-item"><a href="" class="nav-link active">Home</a></li>
+                    <li class="nav-item"><a href="" class="nav-link">Blog</a></li>
+                    <li class="nav-item"><a href="" class="nav-link">Services</a></li>
+                    <li class="nav-item"><a href="" class="nav-link">About us</a></li>
+                </ul>
+                <ul class="navbar-nav gap-3">
+                    <li class="nav-item"><a href="login.php" class="nav-link">Login</a></li>
+                    <li class="nav-item"><a href="register.php"
+                            class="btn btn-primary rounded-pill fw-medium px-4 py-2">Register</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
---
+++ php/index.php
@@ -0,0 +1,229 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=\, initial-scale=1.0">
+    <link rel="stylesheet" href="../assets/css/style.css">
+    <link rel="stylesheet" href="../assets/vendors/bootstrap/css/bootstrap.min.css">
+    <title>blog</title>
+</head>
+
+<body>
+
+    <nav class="navbar navbar-expand-lg bg-white py-3">
+        <div class="container">
+            <a href="" class="navbar-brand text-uppercase">Blogger</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#NavMenu">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navMenu">
+                <ul class="navbar-nav gap-3 mx-auto">
+                    <li class="nav-item"><a href="" class="nav-link active">Home</a></li>
+                    <li class="nav-item"><a href="" class="nav-link">Blog</a></li>
+                    <li class="nav-item"><a href="" class="nav-link">Services</a></li>
+                    <li class="nav-item"><a href="" class="nav-link">About us</a></li>
+                </ul>
+                <ul class="navbar-nav gap-3">
+                    <li class="nav-item"><a href="login.php" class="nav-link">Login</a></li>
+                    <li class="nav-item"><a href="register.php"
+                            class="btn btn-primary rounded-pill fw-medium px-4 py-2">Register</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <!-- header -->
+    <header class="py-5">
+        <div class="container">
+            <h1 class="header-title text-primary text-center text-capitalize">Discover Nice Articles here</h1>
+            <p class="text-secondary text-center text-capitalize"> all teh articles and contents of the site have been
+                update today and you cand find your articles and contents quickly and without any problems</p>
+            <form action="" method="get" class="d-flex align-items-center gap-2 bg-light rounded p-2">
+                <div class="input-group">
+                    <span class="input-group-text bg-transparent border-0">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                            class="bi bi-search" viewBox="0 0 16 16">
+                            <path
+                                d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
+                        </svg>
+                    </span>
+                    <input type="search" name="" id="search" class="form-control border-0" placeholder="search">
+                </div>
+                <button type="submit" class="btn btn-primary">Go</button>
+            </form>
+        </div>
+    </header>
+    <!-- end header -->
+
+    <!-- about section  -->
+    <!-- about -->
+    <section class="py-5">
+        <div class="container">
+            <a href="#" class="row align-items-center hero-post">
+                <div class="col-md-6">
+                    <img src="../assets/img/bradley-lembach-76RsgeCiekY-unsplash.jpg" alt="" class="rounded-3 mb-3 hero-post-img">
+                </div>
+                <div class="col-md-6">
+                    <span class="bg-light rounded p-2 fw-bold text-primary category">The Newest</span>
+                    <h2 class="hero-post-title text-primary text-capitalize mt-3">
+                    I'm an ER doctor: Here's what I found when I asked ChatGPT to diagnose my patients
+                    </h2>
+                    <p class="text-secondary">
+                    ChatGPT recently passed the U.S. Medical Licensing Exam,
+                    but using it for a real-world medical diagnosis would quickly turn deadly.
+                    </p>
+                    <div class="d-flex align-items-center gap-3">
+                        <img src="../assets/img/avatar.jpg" class="avatar rounded-circle" alt="">
+                        <div class="profile">
+                            <p class="m-0 text-primary">Arjuna Kurniawan</p>
+                            <p class="m-0 text-secondary" style="font-size: 14px;">Web Developer</p>
+                        </div>
+                    </div>
+                </div>
+            </a>
+        </div>
+    </section>
+
+    <section class="py-5">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4">
+                    <a href="#" class="card card-post border-0 rounded-3 mb-3 shadow-sm">
+                        <img src="../assets/img/post1.jpg" alt="" class="card-img-top">
+
+                        <div class="card-body">
+                        <span class="bg-light rounded p-2 fw-bold text-primary category">The Newest</span>
+                        <h2 class="card-post-title text-primary text-capitalize mt-3">
+                        I'm an ER doctor: Here's what I found when I asked ChatGPT to diagnose my patients
+                        </h2>
+                        <p class="text-secondary">
+                        ChatGPT recently passed the U.S. Medical Licensing Exam,
+                        but using it for a real-world medical diagnosis would quickly turn deadly.
+                        </p>
+                        <div class="d-flex align-items-center gap-3">
+                            <img src="../assets/img/avatar.jpg" class="avatar rounded-circle" alt="">
+                            <div class="profile">
+                                <p class="m-0 text-primary">Arjuna kurniawan</p>
+                                <p class="m-0 text-secondary" style="font-size: 14px;">Web Developer</p>
+                           </div>
+                        </div>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-md-4">
+                    <a href="#" class="card card-post border-0 rounded-3 mb-3">
+                        <img src="../assets/img/post1.jpg" alt="" class="card-img-top">
+
+                        <div class="card-body">
+                        <span class="bg-light rounded p-2 fw-bold text-primary category">The Newest</span>
+                        <h2 class="card-post-title text-primary text-capitalize mt-3">
+                        I'm an ER doctor: Here's what I found when I asked ChatGPT to diagnose my patients
+                        </h2>
+                        <p class="text-secondary">
+                        ChatGPT recently passed the U.S. Medical Licensing Exam,
+                        but using it for a real-world medical diagnosis would quickly turn deadly.
+                        </p>
+                        <div class="d-flex align-items-center gap-3">
+                            <img src="../assets/img/avatar.jpg" class="avatar rounded-circle" alt="">
+                            <div class="profile">
+                                <p class="m-0 text-primary">Arjuna Kurniawan</p>
+                                <p class="m-0 text-secondary" style="font-size: 14px;">Web Developer</p>
+                           </div>
+                        </div>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-md-4">
+                    <a href="#" class="card card-post border-0 rounded-3 mb-3">
+                        <img src="../assets/img/post1.jpg" alt="" class="card-img-top">
+
+                        <div class="card-body">
+                        <span class="bg-light rounded p-2 fw-bold text-primary category">The Newest</span>
+                        <h2 class="card-post-title text-primary text-capitalize mt-3">
+                        I'm an ER doctor: Here's what I found when I asked ChatGPT to diagnose my patients
+                        </h2>
+                        <p class="text-secondary">
+                        ChatGPT recently passed the U.S. Medical Licensing Exam,
+                        but using it for a real-world medical diagnosis would quickly turn deadly.
+                        </p>
+                        <div class="d-flex align-items-center gap-3">
+                            <img src="../assets/img/avatar.jpg" class="avatar rounded-circle" alt="">
+                            <div class="profile">
+                                <p class="m-0 text-primary">M.Edo Nouvalio</p>
+                                <p class="m-0 text-secondary" style="font-size: 14px;">Web Developer</p>
+                           </div>
+                        </div>
+                        </div>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- end about -->
+   
+    <section class="posts py-5">
+    <div class="container">
+        <div class="row">
+                <div class="col-md-4">
+                    <a href="" class="card border-0 rounded-4 mb-3">
+                        <img src="assets/img/ alt="" class="card-img-top">
+                        <div class="card-body">
+                            <span class="bg-light fw-bold rounded p-2 text-primary category">Developer</span>
+                            <h2 class="text-primary post-title text-capitalize mt-3">Arjuna Kurniawan</h2>
+                            <div class="d-flex align-items-center gap-3">
+                                <img src="../assets/img/" class="avatar rounded-circle" alt="">
+                                <div class="profile">
+                                    <p class="m-0 name text-primary">Arjuna Kurniawan</p>
+                                    <p class="m-0 position text-secondary">Website Developer</p>
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+        </div>
+    </div>
+</section>
+
+<section class="bg-primary subscribe">
+    <div class="container">
+        <div class="row align-items-center justify-content-between">
+            <div class="col-md-6">
+                <h2 class="section-title text-white text-capitalize">Subscribe for new content</h2>
+                <p class="mb-3 mb-md-0 text-secondary text-capitalize">By becoming a member of our blog, you have access articles and content</p>
+            </div>
+            <div class="col-md-5">
+                <form action="#" method="post" class="d-flex align-items-center gap-2">
+                    <div class="w-75">
+                        <div class="input-group rounded p-2">
+                            <span class="input-group-text bg-transparent border-0 text-white"><i class="bi bi-envelope"></i></span>
+                            <input type="email" class="form-control bg-transparent border-0 text-white" placeholder="Enter your email">
+                        </div>
+                    </div>
+                    <button class="btn btn-white w-25 fw-bold">Subscribe</button>
+                </form>
+            </div>
+        </div>
+    </div>
+</section>
+
+<footer class="py-5">
+    <div class="container">
+        <div class="mb-3">
+            <div class="row align-items-center justify-content-beetween">
+                <div class="col-md-4">
+                    <div class="list-unstyled d-flex align-items-center justify-content-center justify-content-md-start gap-3">
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
+
+    <script src="../assets/vendors/bootstrap/js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
